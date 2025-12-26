1716f81af8379ecdd4cc7734056ecd6a1f18cad5
106210740 <106210740@student.swin.edu.au>
2025-12-26 15:54:01 +0700

    module 1 bai tap enter your name

---
+++ src/Lesson1_IntroToJava/EnterYourName.java
@@ -0,0 +1,12 @@
+package Lesson1_IntroToJava;
+
+import java.util.Scanner;
+
+public class EnterYourName {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
