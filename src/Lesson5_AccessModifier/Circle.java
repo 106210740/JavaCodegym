3a24585297833bcabeb9befefe694a5f7895d39e
106210740 <106210740@student.swin.edu.au>
2026-01-07 10:51:34 +0700

    update bt1

--- src/Lesson5_AccessModifier/Circle.java
+++ src/Lesson5_AccessModifier/Circle.java
@@ -1,13 +1,52 @@
 package Lesson5_AccessModifier;
 
-public class Circle {
-    static final double PI = 3.14;
-    double radius;
+//public class Circle {
+//    static final double PI = 3.14;
+//    double radius;
+//
+//    public Circle(double radius) {
+//        this.radius = radius;
+//    }
+//
+//    public double getArea() {
+//        return PI * radius * radius;
+//    }
+//}
 
-    public Circle(double radius){
+//public class Circle {
+//    private double radius = 1.0;
+//    private String color = "red";
+//}
+//public Circle() {
+//
+//}
+//public Circle(double radius){
+//    this.radius = radius;
+//}
+//public double getRadius() {
+//    return radius;
+//}
+//public double getArea() {
+//    return Math.PI * radius * radius;
+//}
+
+public class Circle {
+
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
         this.radius = radius;
     }
-    public double getArea(){
-        return PI * radius * radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
     }
 }
